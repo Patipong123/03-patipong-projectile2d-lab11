de1bdc273d088c9e62cf25862eaa1d58730cf77e
Patipong123 <patipong.saet@bumail.net>
2025-04-29 01:44:36 +0700

    add projectile2d scripts

---
+++ Assets/Scripts/Projectile2d.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Projectile2d : MonoBehaviour
+{
+
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefab;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
